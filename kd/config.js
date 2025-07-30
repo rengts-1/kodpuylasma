4d164fa77ad9d003351086dad1dda794ddb27239
rengts-1 <kaganburge78@gmail.com>
2025-07-30 04:30:47 +0300

    Update config.js

--- kd/config.js
+++ kd/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  token: "MTM2MzUxNjYyMzM4NTc5MjUxMg.GIFBBn.jWx-nZbkQtBxpWLkBboDcCRi1Z",
+  token: "MTM2MzUxNjYyMzM4NTc5MjUxMg.GIFBBn.jWx-nZbkQtBxpWLkBboDcCRi1",
   prefix: "."
 };
